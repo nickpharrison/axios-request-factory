c72cd5c55c670a95f89fae3b927089b016ee0ea4
Nick Harrison <42382362+nickpharrison@users.noreply.github.com>
2024-04-20 14:39:48 +0100

    Identifier and callback changes

--- src/axios-request-factory.js
+++ src/axios-request-factory.js
@@ -28,6 +28,8 @@ class AxiosRequestFactory {
 
 		this.axios = axios.create(this._opts?.axiosInstanceOptions);
 
+		this._id = createUniqueId(this._opts?.id, this._opts?.axiosInstanceOptions);
+
 	}
 
 	addQueueItemToQueue(obj) {
@@ -117,7 +119,7 @@ class AxiosRequestFactory {
 			this._rateLimitUntil = newRateLimit + rateLimitSafetyPeriod;
 			this.logger.warn({
 				message: `Applying rate limit to AxiosRequestFactory`,
-				request_factory_id: `${this._opts.id}`,
+				request_factory_id: `${this._id}`,
 				millis: millis + rateLimitSafetyPeriod
 			});
 		}
@@ -151,51 +153,60 @@ class AxiosRequestFactory {
 		let next;
 		let countAsAttemptOnFailure = true;
 
+		// Wait for any rate limiting to finish
+		await this._waitForRateLimit();
+
+		// Check to see if we already have the maximum number of requests going on
+		const [maxOngoingRequests] = await Promise.all([
+			getValue(this._opts?.maxOngoingRequests)
+		]);
+		if (maxOngoingRequests != null && this._currentOngoingRequests >= maxOngoingRequests) {
+			return;
+		}
+
+		// Check to see if there's actually another request to do (I know we checked before, but in case it changed after the awaits)
+		next = this.getNextQueueItem();
+		if (next == null) {
+			return;
+		}
+		this._currentOngoingRequests += 1;
+
+		// Create headers object if it doesn't exist and make a shorthand for it
+		if (next.axiosConfig.headers == null) {
+			next.axiosConfig.headers = {};
+		}
+		const headers = next.axiosConfig.headers;
+
+		// Get any variables
+		const [authHeader] = await Promise.all([
+			headers['Authorization'] === undefined ? getValue(this._opts?.authHeader) : null, // Don't bother fetching the authHeader if we already have "Authorization" header set
+		]);
+
+		// Set the authorization header if we got one back
+		if (authHeader) {
+			headers['Authorization'] = authHeader;
+		}
+
+		// Execute callbacks
+		await next.options?.beforeExec?.({
+			axiosConfig: next.axiosConfig,
+			resp: resp,
+			previousAttempts: next.failedAttempts ?? 0,
+		});
+
+		await this._opts?.beforeExec?.({
+			axiosConfig: next.axiosConfig,
+			previousAttempts: next.failedAttempts ?? 0,
+		});
+
+		// Wait again for any rate limiting to finish because some might have been introduced since we checked before
+		await this._waitForRateLimit();
+
+		if (global.axios_request_factory_debug || this._opts?.debug) {
+			console.log(`[ARF#${this._id}] Start: ${next.axiosConfig.method ?? 'GET'} ${next.axiosConfig.baseURL ?? ''}${next.axiosConfig.url}`);
+		}
+
 		try {
-
-			// Wait for any rate limiting to finish
-			await this._waitForRateLimit();
-
-			// Check to see if we already have the maximum number of requests going on
-			const [maxOngoingRequests] = await Promise.all([
-				getValue(this._opts?.maxOngoingRequests)
-			]);
-			if (maxOngoingRequests != null && this._currentOngoingRequests >= maxOngoingRequests) {
-				return;
-			}
-
-			// Check to see if there's actually another request to do (I know we checked before, but in case it changed after the awaits)
-			next = this.getNextQueueItem();
-			if (next == null) {
-				return;
-			}
-			this._currentOngoingRequests += 1;
-
-			// Create headers object if it doesn't exist and make a shorthand for it
-			if (next.axiosConfig.headers == null) {
-				next.axiosConfig.headers = {};
-			}
-			const headers = next.axiosConfig.headers;
-
-			// Get any variables
-			const [authHeader] = await Promise.all([
-				headers['Authorization'] === undefined ? getValue(this._opts?.authHeader) : null, // Don't bother fetching the authHeader if we already have "Authorization" header set
-			]);
-
-			// Set the authorization header if we got one back
-			if (authHeader) {
-				headers['Authorization'] = authHeader;
-			}
-
-			// Wait again for any rate limiting to finish because some might have been introduced since we checked before
-			await this._waitForRateLimit();
-
-			const preExecuteCallback = next.options.preExecuteCallback
-			if (typeof preExecuteCallback === 'function') {
-				await preExecuteCallback({
-					previousAttempts: next.failedAttempts ?? 0,
-				});
-			}
 
 			// Make the actual request
 			resp = await this.axios(next.axiosConfig);
@@ -207,6 +218,31 @@ class AxiosRequestFactory {
 			err = e;
 
 		}
+
+		if (global.axios_request_factory_debug || this._opts?.debug) {
+			if (errored) {
+				console.error(`[ARF#${this._id}] Error: ${next.axiosConfig.method ?? 'GET'} ${next.axiosConfig.baseURL ?? ''}${next.axiosConfig.url}`);
+			} else {
+				console.log(`[ARF#${this._id}]  Done: ${next.axiosConfig.method ?? 'GET'} ${next.axiosConfig.baseURL ?? ''}${next.axiosConfig.url}`);
+			}
+		}
+
+		// Execute callbacks
+		await this._opts?.afterExec?.({
+			errored: errored,
+			error: err,
+			axiosConfig: next.axiosConfig,
+			resp: resp,
+			previousAttempts: next.failedAttempts ?? 0,
+		});
+
+		await next.options?.afterExec?.({
+			errored: errored,
+			error: err,
+			axiosConfig: next.axiosConfig,
+			resp: resp,
+			previousAttempts: next.failedAttempts ?? 0,
+		});
 
 		const reattemptOn = (await getValue(this._opts?.reattemptOn)) ?? ['NoStatusCode'];
 
@@ -254,7 +290,7 @@ class AxiosRequestFactory {
 
 				this.logger.error({
 					message: 'An error occurred when cleaning up the end of a AxiosRequestFactory request',
-					req_factory_id: `${this._opts.id}`,
+					req_factory_id: `${this._id}`,
 					cause: err
 				});
 
@@ -281,11 +317,62 @@ class AxiosRequestFactory {
 		this._trigger().catch((err) => {
 			this.logger.error({
 				message: 'An error occurred during a AxiosRequestFactory trigger',
-				req_factory_id: `${this._opts.id}`,
+				req_factory_id: `${this._id}`,
 				cause: err
 			});
 		});
 
+	}
+
+}
+
+const randomString = (length) => {
+    let output = '';
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    while (output.length < length) {
+		output += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return output;
+}
+
+const usedIds = [];
+
+const createId = (axiosInstanceOpts) => {
+
+	const urlString = axiosInstanceOpts?.url ?? axiosInstanceOpts?.baseURL;
+
+	if (urlString) {
+		const url = new URL(urlString);
+		if (url.hostname) {
+			return url.hostname;
+		}
+	}
+
+	return randomString(16);
+
+}
+
+const createUniqueId = (id, axiosInstanceOpts) => {
+
+	const rawId = id ?? createId(axiosInstanceOpts);
+
+	const stringId = `${rawId}`.replace(/#/g, '_');
+
+	if (!usedIds.includes(stringId)) {
+		usedIds.push(stringId);
+		return stringId;
+	}
+
+	let number = 0;
+	while (true) {
+		if (++number > 1000) {
+			throw new Error(`Infinite loop detected setting ID`);
+		}
+		const test = `${stringId}#${number}`;
+		if (!usedIds.includes(test)) {
+			usedIds.push(test);
+			return test;
+		}
 	}
 
 }
