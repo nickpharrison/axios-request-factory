1f9ac11902d51951ff333427366c05ee9cf338d6
Nick Harrison <42382362+nickpharrison@users.noreply.github.com>
2024-09-23 18:04:28 +0100

    Add additional parameters to types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,14 +1,26 @@
 
 import { AxiosRequestConfig, AxiosResponse } from 'axios';
 
+type Functionisable<T> = T|(() => T|Promise<T>);
+
 interface AxiosRequestFactoryConstructorOptions {
 	id?: string;
 	axiosInstanceOptions: AxiosRequestConfig;
 	logger?: any;
+	authHeader?: Functionisable<string>
+	maxAttempts?: Functionisable<number>;
+	rateLimitSafetyPeriod?: number;
+	maxOngoingRequests?: Functionisable<number>;
+	debug?: boolean;
+	reattemptOn?: Functionisable<(number|'NoStatusCode')[]>;
+	beforeExec?: ({axiosConfig: AxiosRequestConfig, previousAttempts: number}) => void|Promise<void>;
+	afterExec?: ({errored: boolean, error: any, axiosConfig: AxiosRequestConfig, resp: AxiosResponse, previousAttempts: number}) => void|Promise<void>;
 }
 
 interface AxiosRequestFactoryRequestOptions {
 	priority?: number;
+	beforeExec?: ({axiosConfig: AxiosRequestConfig, previousAttempts: number}) => void|Promise<void>;
+	afterExec?: ({errored: boolean, error: any, axiosConfig: AxiosRequestConfig, resp: AxiosResponse, previousAttempts: number}) => void|Promise<void>;
 }
 
 export class AxiosRequestFactory {
