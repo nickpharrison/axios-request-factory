46c42102f10bf9c3276269c98c61b06059327921
Nick Harrison <42382362+nickpharrison@users.noreply.github.com>
2024-09-23 17:44:56 +0100

    Add Optional Types

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,17 +2,19 @@
 import { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 type AxiosRequestFactoryRequestOptions = {
+	id?: string;
 	axiosInstanceOptions: AxiosRequestConfig;
+	logger?: any;
 }
 
 type AxiosRequestFactoryConstructorOptions = {
-	priority: number;
+	priority?: number;
 }
 
 export class AxiosRequestFactory {
 
-	constructor(options: AxiosRequestFactoryConstructorOptions)
+	constructor(options?: AxiosRequestFactoryConstructorOptions)
 
-	request(axiosConfig: AxiosRequestConfig, options: AxiosRequestFactoryOptions): Promise<AxiosResponse>
+	request(axiosConfig: AxiosRequestConfig, options?: AxiosRequestFactoryOptions): Promise<AxiosResponse>
 
 }
